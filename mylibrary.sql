dd16c1e0e47e5d73f6c1dc2fa0a876caa50f9592
Maximus <62805287+PapyRGB@users.noreply.github.com>
2023-12-19 12:24:44 +0100

    Update mylibrary.sql

--- mylibrary.sql
+++ mylibrary.sql
@@ -39,6 +39,21 @@ CREATE TABLE `admin` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `FirstName` varchar(100) NOT NULL,
+  `LastName` varchar(100) NOT NULL,
+  `Mail` varchar(150) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Phone` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `author`
 --
 
