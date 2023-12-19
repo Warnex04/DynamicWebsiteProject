ea64cb63498fee98881c8b110bd872f49af59757
Maximus <62805287+PapyRGB@users.noreply.github.com>
2023-12-19 12:02:22 +0100

    update

--- templates/sign_in-up/dashboard.php
+++ templates/sign_in-up/dashboard.php
@@ -1,10 +1,17 @@
 <?php
     session_start();
     if (!isset($_SESSION['username'])) {
-        header('Location: login.php');
+        header('Location: sign-in.php');
     exit();
     }
     $username = $_SESSION['username'];
+
+    include('db.php');
+    // Récupérer tous les utilisateurs
+    $query = "SELECT id, username FROM users";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 
@@ -18,7 +25,20 @@
 </head>
 
 <body>
-    <h2>Welcome, <?php echo $username; ?>!</h2>
+    <h2>Welcome, <?php echo $_SESSION['username']; ?>!</h2>
     <a href="logout.php">Logout</a>
+    <a href="change_password.php">Modifier le mot de passe</a>
+
+    <h2>Liste des Utilisateurs</h2>
+    <form action="change_other_user_password.php" method="get">
+        <label for="user_id">Sélectionnez un utilisateur :</label>
+        <select name="user_id" id="user_id">
+            <?php foreach ($users as $user): ?>
+                <option value="<?php echo $user['id']; ?>"><?php echo htmlspecialchars($user['username']); ?></option>
+            <?php endforeach; ?>
+        </select>
+        <button type="submit">Modifier le Mot de Passe</button>
+    </form>
 </body>
 </html>
+
--- templates/sign_in-up/sign-in.php
+++ templates/sign_in-up/sign-in.php
@@ -50,7 +50,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </form>
     <a href="sign-up.php">Enregistrer</a>
 
- 
-
 </body>
 </html>
+
