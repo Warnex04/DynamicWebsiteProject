2b417f74f0c9e3e04e63003bac3fd4cea30324a9
Warnex <mohamadmbaker@gmail.com>
2023-12-20 14:36:20 +0100

    Update admin_dashboard.php and style.css

--- admin_dashboard/admin_dashboard.php
+++ admin_dashboard/admin_dashboard.php
@@ -10,7 +10,7 @@ $_SESSION['username'] = 'FakeAdmin';
 // Check if the user is logged in and has the 'admin' role
 if (!isset($_SESSION['logged_in']) || $_SESSION['role'] !== 'admin') {
     // User is not logged in as an admin, redirect to the login page
-    header('Location: login.php');
+    header('Location: ../login/login.php');
     exit;
 }
 
@@ -30,6 +30,25 @@ try {
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+
+function fetchTableData($conn, $tableName) {
+    try {
+        $stmt = $conn->prepare("SELECT * FROM " . $tableName);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        // Handle error appropriately
+        // For debugging purposes, you can uncomment the line below
+        // echo "Error: " . $e->getMessage();
+        return []; // Return an empty array to prevent further errors
+    }
+}
+
+// Fetch data from all tables
+$admins = fetchTableData($conn, 'admin');
+$authors = fetchTableData($conn, 'author');
+$books = fetchTableData($conn, 'book');
+$ecrits = fetchTableData($conn, 'ecrit');
 ?>
 
 
@@ -99,10 +118,124 @@ try {
                 </tbody>
             </table>
         </section>
+        <div class="dashboard-grid">
+        <!-- Admin Table Section -->
+        <section class="table-container">
+        <h2>Admins</h2>
+        <div class="scrollable-table">
+            <!-- Repeat the following table structure for each table -->
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($admins as $row): ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($row['ID']); ?></td>
+                            <td><?php echo htmlspecialchars($row['FirstName']); ?></td>
+                            <td><?php echo htmlspecialchars($row['LastName']); ?></td>
+                            <td><?php echo htmlspecialchars($row['Mail']); ?></td>
+                            <td><?php echo htmlspecialchars($row['Phone']); ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </section>
+
+<!-- Author Table Section -->
+<section class="table-container">
+    <h2>Authors</h2>
+    <div class="scrollable-table">
+        <table>
+            <thead>
+                <tr>
+                    <th>Num</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Birthdate</th>
+                    <th>Nationality</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($authors as $row): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($row['Num']); ?></td>
+                        <td><?php echo htmlspecialchars($row['FirstName']); ?></td>
+                        <td><?php echo htmlspecialchars($row['LastName']); ?></td>
+                        <td><?php echo htmlspecialchars($row['BirthDate']); ?></td>
+                        <td><?php echo htmlspecialchars($row['Nationality']); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</section>
+
+<!-- Book Table Section -->
+<section class="table-container">
+    <h2>Books</h2>
+    <div class="scrollable-table">
+        <table>
+            <thead>
+                <tr>
+                    <th>ISSN</th>
+                    <th>Title</th>
+                    <th>Summary</th>
+                    <th>Number of Pages</th>
+                    <th>Category</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($books as $row): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($row['ISSN']); ?></td>
+                        <td><?php echo htmlspecialchars($row['Title']); ?></td>
+                        <td><?php echo htmlspecialchars($row['Summary']); ?></td>
+                        <td><?php echo htmlspecialchars($row['NbPages']); ?></td>
+                        <td><?php echo htmlspecialchars($row['Category']); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</section>
+
+<!-- Ecrit Table Section -->
+<section class="table-container">
+    <h2>Ecrit</h2>
+    <div class="scrollable-table">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Author Num</th>
+                    <th>Book ISSN</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($ecrits as $row): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($row['ID']); ?></td>
+                        <td><?php echo htmlspecialchars($row['Num']); ?></td>
+                        <td><?php echo htmlspecialchars($row['ISSN']); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</section>
+                </div>
         
     </main>
     <footer>
-        <p>&copy; 2023 Library Dashboard</p>
+        <p>&copy; 2023 Library</p>
     </footer>
 </body>
 </html>
