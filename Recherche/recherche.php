693ab46d2fb8023df0a25e8be8a3ec8ae2f9d0ed
Mohammed ADDI <132593906+ai-mohammed@users.noreply.github.com>
2023-12-29 17:33:35 +0100

    D 29

--- Recherche/recherche.php
+++ Recherche/recherche.php
@@ -142,12 +142,148 @@ function processSearch($pdo, $searchString, $filters) {
     
     <style>
 
-.search-form {
-    display: flex;
-    justify-content: center;
-    margin-top: -2%;
+/* Header styles */
+header {
+    background-color: #005f73; /* Deep blue background */
+    color: white;
+    padding: 1rem 2rem; /* Increased padding for better spacing */
+    display: flex; /* Flexbox for layout */
+    align-items: center; /* Align items vertically */
+    justify-content: space-between; /* Space between elements */
 }
 
+/* Navigation styles */
+nav ul {
+    list-style: none;
+    padding: 0;
+    display: flex; /* Flexbox for horizontal layout */
+    margin: 0;
+}
+
+nav ul li {
+    margin-right: 20px;
+}
+
+nav a {
+    color: white;
+    text-decoration: none;
+    padding: 0.5rem 1rem; /* Padding for clickable area */
+    transition: background-color 0.3s; /* Smooth transition for hover effect */
+}
+
+nav a:hover {
+    background-color: #00a896; /* Slightly lighter blue on hover */
+}
+
+
+/* button des filtres  */
+.setting-btn {
+            width: 25px;
+            height: 25px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            gap: 3px;
+            background-color: #94d2bd;
+            border-radius: 10px;
+            cursor: pointer;
+            border: none;
+            box-shadow: 0px 0px 0px 2px rgb(212, 209, 255);
+            transition: background-color 0.3s; /* Smooth transition for hover effect */
+        }
+        
+        .bar {
+            width: 50%;
+            height: 2px;
+            background-color: rgb(229, 229, 229);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: relative;
+            border-radius: 2px;
+        }
+
+        .bar::before {
+            content: "";
+            width: 2px;
+            height: 2px;
+            background-color: rgb(126, 117, 255);
+            position: absolute;
+            border-radius: 50%;
+            border: 2px solid white;
+            transition: all 0.3s;
+            box-shadow: 0px 0px 5px white;
+        }
+
+        .bar1::before {
+            transform: translateX(-4px);
+        }
+
+        .bar2::before {
+            transform: translateX(4px);
+        }
+
+        .setting-btn:hover .bar1::before {
+            transform: translateX(4px);
+        }
+
+        .setting-btn:hover .bar2::before {
+            transform: translateX(-4px);
+        }
+
+
+
+/* Search bar styles */
+#search-bar-container {
+    flex-grow: 1; /* Allow the container to grow */
+    display: flex;
+    justify-content: center; /* Center the search bar */
+}
+
+#search-bar {
+    display: flex;
+    align-items: center;
+}
+
+#search-text {
+    padding: 0.5rem;
+    width: 200px;
+    border-radius: 5px; /* Rounded corners */
+    border: 1px solid #ddd; /* Light border */
+    margin-right: 10px;
+    color: black;
+}
+
+#search-button {
+    padding: 0.5rem 1rem;
+    background-color: #94d2bd; /* Light green background */
+    border: none;
+    border-radius: 5px; /* Rounded corners */
+    color: white;
+    cursor: pointer;
+    transition: background-color 0.3s; /* Smooth transition for hover effect */
+}
+
+#search-button:hover {
+    background-color: #82bfb9; /* Slightly darker green on hover */
+}
+
+
+/* Login button styles */
+#login-button {
+    padding: 0.5rem 1rem;
+    background-color: #00a896; /* Matching the hover color of nav links */
+    border: none;
+    border-radius: 5px; /* Rounded corners */
+    color: white;
+    cursor: pointer;
+    transition: background-color 0.3s; /* Smooth transition for hover effect */
+}
+
+#login-button:hover {
+    background-color: #007f7f; /* Slightly darker shade for hover */
+}
 
 
 
@@ -181,48 +317,15 @@ function processSearch($pdo, $searchString, $filters) {
 }
 
 .main-content.active {
-    margin-left: 250px; /* Make room for sidebar */
+    margin-left: 200px; /* Make room for sidebar */
     width: calc(100% - 200px); /* Decrease width */
-    margin-top: 5%;
+    margin-top: 0%;
 }
 
 #msbo.active {
     position: fixed;
     left: 200px; /* Déplacez le bouton vers la droite avec la sidebar */
     transition: left 0.5s ease; /* Animez le mouvement du bouton */
-}
-
-
-button {
-  width: fit-content;
-  min-width: 100px;
-  height: 45px;
-  padding: 8px;
-  border-radius: 5px;
-  border: 2.5px solid #E0E1E4;
-  box-shadow: 0px 0px 20px -20px;
-  cursor: pointer;
-  background-color: white;
-  transition: all 0.2s ease-in-out 0ms;
-  user-select: none;
-  font-family: 'Poppins', sans-serif;
-  
-}
-
-
-button#filter-button {
-    position: absolute; /* Position relative to main-content because it's set to relative */
-    top: 5%; /* Adjust as needed */
-    left: 1%; /* Adjust as needed */
-   }
-
-button:hover {
-  background-color: #F2F2F2;
-  box-shadow: 0px 0px 20px -18px;
-}
-
-button:active {
-  transform: scale(0.95);
 }
 
 
@@ -304,19 +407,144 @@ button:active {
 .search-bar-input:focus {
     outline: none;
 }
+
+
+
+/* button fermer */
+
+.button {
+  position: relative;
+  width: 1.5em;
+  height: 1.5em;
+  border: none;
+  background: rgba(180, 83, 107, 0.4);
+  border-radius: 5px;
+  transition: background 0.5s;
+}
+
+.X {
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 1em;
+  height: 1.5px;
+  background-color: rgb(255, 255, 255);
+  transform: translateX(-50%) rotate(45deg);
+}
+
+.Y {
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 1em;
+  height: 1.5px;
+  background-color: #fff;
+  transform: translateX(-50%) rotate(-45deg);
+}
+
+
+.button:hover {
+  background-color: rgb(211, 21, 21);
+}
+
+.button:active {
+  background-color: rgb(130, 0, 0);
+}
+
+
+
+
+/* reset button */
+
+#reset-filters-button {
+  width: 23px;
+  height: 23px;
+  border-radius: 50%;
+  background-color: rgb(20, 20, 20);
+  border: none;
+  font-weight: 600;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.164);
+  cursor: pointer;
+  transition-duration: 0.3s;
+  overflow: hidden;
+  position: absolute;
+  top: 6.5%;
+  left: 70%; 
+  gap: 2px;
+}
+
+#reset-filters-button .svgIcon {
+  width: 10px;
+  transition-duration: 0.3s;
+}
+
+#reset-filters-button .svgIcon path {
+  fill: white;
+}
+
+#reset-filters-button:hover {
+  transition-duration: 0.3s;
+  background-color: rgb(255, 69, 69);
+  align-items: center;
+  gap: 0;
+}
+
+#reset-filters-button .bin-top {
+  transform-origin: bottom right;
+}
+
+#reset-filters-button:hover .bin-top {
+  transition-duration: 0.5s;
+  transform: rotate(160deg);
+}
+
+
    </style>
 </head>
 <body>
-<button type="button" id="filter-button">
-    Filtres
-</button>
+
+<div class="main-content">
+<header>
+    <nav>
+        <ul>
+        <li>
+        <button class="setting-btn" onclick="toggleSidebar()" id="filter-button">
+                    <span class="bar bar1"></span>
+                    <span class="bar bar2"></span>
+                    <span class="bar bar1"></span>
+                </button>
+            </li>
+            <li><a href="#popular-books">Home </a></li>
+        </ul>
+    </nav>
+    <div id="search-bar-container">
+        <div id="search-bar">
+            <!-- Note that the form action needs to point to the script that processes the search -->
+            <form method="post" action="" class="search-form">
+                <input type="text" id="search-text" name="search" placeholder="Rechercher des livres..." required autocomplete="off">
+                <button type="submit" id="search-button">Rechercher</button>
+            </form>
+        </div>
+    </div>
+    <a id="login-button" href="../login/login.php">Admin panel</a>
+</header>
 
 
     <div class="sidebar">
         <form method="post" action="">
             <input type="hidden" name="searchString" value="<?php echo htmlspecialchars($searchString); ?>">
           
-            <button type="button" id="close-sidebar-button">Fermer</button>
+            <button class="button"  id="close-sidebar-button">
+            <span class="X"></span>
+            <span class="Y"></span>
+
+            </button>
 
             <!-- Options de filtrage -->
             <h3>Filtres:</h3>
@@ -344,24 +572,54 @@ button:active {
                     <label for="nationality_<?php echo htmlspecialchars($nationality); ?>"><?php echo htmlspecialchars($nationality); ?></label>
                 </div>
             <?php endforeach; ?>
+            <button type="button" id="reset-filters-button">
+            <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 69 14"
+    class="svgIcon bin-top"
+  >
+    <g clip-path="url(#clip0_35_24)">
+      <path
+        fill="black"
+        d="M20.8232 2.62734L19.9948 4.21304C19.8224 4.54309 19.4808 4.75 19.1085 4.75H4.92857C2.20246 4.75 0 6.87266 0 9.5C0 12.1273 2.20246 14.25 4.92857 14.25H64.0714C66.7975 14.25 69 12.1273 69 9.5C69 6.87266 66.7975 4.75 64.0714 4.75H49.8915C49.5192 4.75 49.1776 4.54309 49.0052 4.21305L48.1768 2.62734C47.3451 1.00938 45.6355 0 43.7719 0H25.2281C23.3645 0 21.6549 1.00938 20.8232 2.62734ZM64.0023 20.0648C64.0397 19.4882 63.5822 19 63.0044 19H5.99556C5.4178 19 4.96025 19.4882 4.99766 20.0648L8.19375 69.3203C8.44018 73.0758 11.6746 76 15.5712 76H53.4288C57.3254 76 60.5598 73.0758 60.8062 69.3203L64.0023 20.0648Z"
+      ></path>
+    </g>
+    <defs>
+      <clipPath id="clip0_35_24">
+        <rect fill="white" height="14" width="69"></rect>
+      </clipPath>
+    </defs>
+  </svg>
+
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 69 57"
+    class="svgIcon bin-bottom"
+  >
+    <g clip-path="url(#clip0_35_22)">
+      <path
+        fill="black"
+        d="M20.8232 -16.3727L19.9948 -14.787C19.8224 -14.4569 19.4808 -14.25 19.1085 -14.25H4.92857C2.20246 -14.25 0 -12.1273 0 -9.5C0 -6.8727 2.20246 -4.75 4.92857 -4.75H64.0714C66.7975 -4.75 69 -6.8727 69 -9.5C69 -12.1273 66.7975 -14.25 64.0714 -14.25H49.8915C49.5192 -14.25 49.1776 -14.4569 49.0052 -14.787L48.1768 -16.3727C47.3451 -17.9906 45.6355 -19 43.7719 -19H25.2281C23.3645 -19 21.6549 -17.9906 20.8232 -16.3727ZM64.0023 1.0648C64.0397 0.4882 63.5822 0 63.0044 0H5.99556C5.4178 0 4.96025 0.4882 4.99766 1.0648L8.19375 50.3203C8.44018 54.0758 11.6746 57 15.5712 57H53.4288C57.3254 57 60.5598 54.0758 60.8062 50.3203L64.0023 1.0648Z"
+      ></path>
+    </g>
+    <defs>
+      <clipPath id="clip0_35_22">
+        <rect fill="white" height="57" width="69"></rect>
+      </clipPath>
+    </defs>
+  </svg>
+</button>
 
             <input type="submit" value="Appliquer">
         </form>
     </div>
 
-    <div class="main-content">
-        <!-- Formulaire de recherche principal -->
-        <form method="post" action="" class="search-form">
-        <div class="search-bar-container">
-        <div class="search-input-group">
-            <input type="text" class="search-bar-input" placeholder="Recherche..." name="search" required />
-            <button type="submit" class="search-bar-button">Rechercher</button>
-        </div>
-    </div>
-        </form>
-
-        <!-- Résultats de la recherche -->
-        <?php if (!empty($results)): ?>
+   
+    <!-- The main content of your page -->
+    <!-- The search results and any other content you want to include -->
+    <?php if (!empty($results)): ?>
         <h2 class="results-heading"><?php echo count($results); ?> résultat(s) trouvé(s) pour '<?php echo htmlspecialchars($searchString); ?>'</h2>
         <?php foreach ($results as $result): ?>
             <div class="thesis-box clearfix">
@@ -377,25 +635,29 @@ button:active {
         <p class="results-heading">Aucun résultat trouvé pour '<?php echo htmlspecialchars($searchString); ?>'.</p>
     <?php endif; ?>
 </div>
+<!-- jQuery inclusion -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
-  
-    <!-- Inclusion de jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
-    <script>
+<script>
 $(function() {
-    // Gestionnaire d'événements pour ouvrir la sidebar
+    // Event handler for opening the sidebar
     $('#filter-button').click(function() {
         $('.sidebar').toggleClass('active');
         $('.main-content').toggleClass('active');
     });
 
-    // Gestionnaire d'événements pour fermer la sidebar
+    // Event handler for closing the sidebar
     $('#close-sidebar-button').click(function() {
         $('.sidebar').removeClass('active');
         $('.main-content').removeClass('active');
+    });
+
+    // Event handler for the reset filters button
+    $('#reset-filters-button').click(function() {
+        // Uncheck all checkbox filters in the sidebar
+        $('.sidebar input[type="checkbox"]').prop('checked', false);
+
+        // Here we are not resetting text inputs, as per your requirement
     });
 });
 </script>
