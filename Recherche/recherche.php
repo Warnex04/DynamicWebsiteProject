aff00b2ad7c01c76252d5aa9345e2b5ace2c55dc
Maximus <62805287+PapyRGB@users.noreply.github.com>
2024-01-07 00:01:55 +0100

    Update recherche.php

--- Recherche/recherche.php
+++ Recherche/recherche.php
@@ -11,11 +11,11 @@ $nationalities = [];
 // Database connection parameters
 $servername = "localhost"; // usually localhost
 $username = "root";
-//$password = "root";
+$password = "root";
 $database = "mylibrary";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username);//, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
